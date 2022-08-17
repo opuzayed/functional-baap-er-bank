5ddfad70f3bf6183817726240bd6688fd5606081
opuzayed <zayedopu4933@gmail.com>
2022-08-17 23:05:25 +0600

    using function for deposit

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,22 @@
+function getInputFieldValueById(inputFieldId){
+const inputField = document.getElementById(inputFieldId);
+const inputFieldValueString = inputField.value;
+const inputFieldValue = parseFloat(inputFieldValueString);
+inputField.value = '';
+
+return inputFieldValue;
+}
+
+function getElementValueById(elementId)
+{
+   const element = document.getElementById(elementId);
+   const elementValueString = element.innerText;
+   const value = parseFloat(elementValueString);
+
+   return value;
+}
 document.getElementById('btn-deposit').addEventListener('click', function(){
-    console.log('deposit clicked');
+  const newDepositAmount = getInputFieldValueById('deposit-field');
+  const previousDepositTotal = getElementValueById('deposit-total');
+  console.log(newDepositAmount);  
 })
